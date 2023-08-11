0705eda5872077fa3d07720f1e6fe0939a801dca
cheolung12 <109952479+cheolung12@users.noreply.github.com>
2023-08-11 10:16:15 +0900

    study: jest.fn()

--- src/mock/check/test/check.test.js
+++ src/mock/check/test/check.test.js
@@ -1,5 +1,7 @@
 const check = require('../check');
 
+// mocking: 단위 테스트를 작성할 때, 해당 코드가 의존하는 부분을 가짜(mock)로 대체하는 기법
+
 describe('Check', () => {
     let onSuccess;
     let onFail;
@@ -11,8 +13,24 @@ describe('Check', () => {
 
     it('should call onSuccess when predicate is true', () => {
         check(() => true, onSuccess, onFail);
-        expect(onSuccess.mock.call.length).toBe(1);
+        // expect(onSuccess.mock.calls.length).toBe(1);
+        expect(onSuccess).toHaveBeenCalledTimes(1);
+        expect(onFail).toHaveBeenCalledTimes(0);
+        // 이 인자와 함께 호출되어야 한다.
+        expect(onSuccess).toHaveBeenCalledWith('yes')
+        
+        
     });
+
+    it('should call onSuccess when predicate is false', () => {
+        check(() => false, onSuccess, onFail);
+        // expect(onSuccess.mock.calls.length).toBe(1);
+        expect(onFail).toHaveBeenCalledTimes(1);
+        expect(onSuccess).toHaveBeenCalledTimes(0);
+        // 이 인자와 함께 호출되어야 한다.
+        expect(onFail).toHaveBeenCalledWith('no')
+    });
+
 
 
 })
